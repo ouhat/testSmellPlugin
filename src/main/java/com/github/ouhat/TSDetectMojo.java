d62c5086e54fb0c1e4b1e16975b1f42584767ab8
ouhat <o.elhatifi@alumnos.upm.es>
2024-10-27 23:25:05 +0100

    Final version

--- src/main/java/com/github/ouhat/TSDetectMojo.java
+++ src/main/java/com/github/ouhat/TSDetectMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,12 +13,16 @@ import java.io.InputStreamReader;
 @Mojo(name = "detect", defaultPhase = LifecyclePhase.TEST)
 public class TSDetectMojo extends AbstractMojo {
 
+    @Parameter(property = "TsPath", required = true)
+    public String TsPath;
     @Override
     public void execute() throws MojoExecutionException {
         getLog().info("Ejecutando tsDetect para identificar test smells...");
 
+
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder("tsDetect", "-d", "src/test/java");
+            // Ajusta el comando según sea necesario, por ejemplo, con un .jar:
+            ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", TsPath, "-d", "src/test/java");
             Process process = processBuilder.start();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
